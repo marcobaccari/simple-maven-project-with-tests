7f4c6d9a68be5037210e5d5a52c75e4e6694df69
marco.baccari <marco.baccari@CPX-U7XX3C9FNI6.dir.svc.accenture.com>
2019-03-06 19:08:48 +0100

    fake commit

--- src/main/java/test/FakeMain.java
+++ src/main/java/test/FakeMain.java
@@ -2,11 +2,13 @@ package test;
 
 import javax.xml.bind.DataBindingException;
 
+import com.fasterxml.jackson.databind.exc.IgnoredPropertyException;
+
 public class FakeMain {
 
 	public static void main(String[] args) {
 		
-		DataBindingException bindingException = new DataBindingException("eeee", new Throwable());
+		IgnoredPropertyException bindingException = new IgnoredPropertyException(null, "", null, null, null, null);
 		System.out.println("sise" + bindingException);
 
 	}
